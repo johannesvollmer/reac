18641c96f5475e3379e53ff6d12084d72725502c
johannesvollmer <johannes.boenki@t-online.de>
2019-10-02 03:19:11 +0200

    Add hash function

--- reac.js
+++ reac.js
@@ -13,24 +13,24 @@ for (let tag of "nav div a p span input form button label img svg canvas main se
 // add main reac function
 window.reac.run = (root, render, initialstate) => {
     if (root == null)
-        throw "Reac root element is not defined"
+        throw "Reac root element must be defined"
 
-    if (root.childElementCount != 0)
-        throw "Reac root element is not empty"
-
-    // the mutable state of the app, passed to the event listeners
+    // the mutable state of the app, updated only inside event listeners
     const state = initialstate
 
     // keep track of whether we need to re-render
     let stateHasChanged = true
 
     // the currently rendered "virtual dom" of this reac runner
+    // will be compared to the new view in order to only update modified properties
     let currentView = null
     
     repeat(() => {
         if (stateHasChanged) {
             const nextView = render(state) // TODO should be able to return arrays and strings?   // TODO components would allow truly partial updates
-            updateElement(root, { children: [currentView] }, { children: [nextView] })
+            compileView(nextView)
+            
+            root = updateElement(root, currentView, nextView)
 
             currentView = nextView
             stateHasChanged = false
@@ -41,20 +41,17 @@ window.reac.run = (root, render, initialstate) => {
             
 
     function updateElement(native, currentView, newView){
-        // replace object if tag changes
-        if (currentView.tag !== newView.tag){
+        // replace object if none exists yet or if the tag changes
+        if (currentView == null || currentView.tag !== newView.tag){
             const newNative = createNativeElement(newView)
             native.parentNode.replaceChild(newNative, native)
-            return
+            return newNative
         }
         
-        // enable style comparison
-        compileView(newView)
-
         // update existing attributes
         for (let updatedName in newView.attributes){
             const newValue = newView.attributes[updatedName]
-            if (newValue !== currentView.attributes[updatedName]){ // FIXME this will not compare style objects
+            if (newValue !== currentView.attributes[updatedName]){ 
                 native[updatedName] = createNativeAttribute(updatedName, newValue)
                 // console.log("updated attribute", updatedName, newValue)
             }
@@ -71,14 +68,14 @@ window.reac.run = (root, render, initialstate) => {
         if (!hasChildren(newView)){
             if (hasChildren(currentView))   
                 native.innerHTML = ""
-        } 
+        }
 
         // otherwise, handle complex scenarios         
         else {
             // TODO use something smarter which allows better reusage where children are removed
-            for (let index = 0; index < newView.children.length; index++){
+            for (let index = 0; index < newView.children.length; index++){ // we know children are defined and not empty
                 const newChild = newView.children[index]
-                const currentChild = currentView.children[index]
+                const currentChild = (currentView.children || []) [index]
 
                 if (currentChild == undefined){
                     native.appendChild(createNativeElement(newChild))
@@ -94,15 +91,43 @@ window.reac.run = (root, render, initialstate) => {
             while(native.childElementCount > newView.children.length)
                 native.removeChild(native.lastChild)
         }
+
+        return native
     }
 
-    // converts object attributes to string
     function compileView(view){
         // convert style to string to enable attribute comparison
         if (view.attributes != undefined && typeof view.attributes.style === "object"){ // also checks for undefined
             view.attributes.style = Object.entries(view.attributes.style)
                 .map(([key, value]) => key + ": " + value).join("; ")
         }
+
+        // compute content hash
+        view.hash = (view.tag || "?").toString() + (view.children || []).length.toString()
+
+        for (let child of view.children || []){
+            compileView(child)
+
+            if (view.hash.length < 60)
+                view.hash += child.hash
+        }
+        
+        for(let attribute in view.attributes || {}){
+            if (view.hash.length > 70) break
+
+            const value = view.attributes[attribute].toString() || "-"
+            if (!attribute.startsWith("on"))
+                view.hash += attribute.charAt(0) + value.charAt(value.length / 2)
+        }
+
+        if (hasChildren(view)){
+            view.childCache = {}
+            for(let index = 0; index < view.children.length; index++)
+                view.childCache[view.children[index].hash] = index
+        }
+
+        console.log("hashed ", view)
+        return view.hash
     }
 
     /// will only be called on realizing change, not on render
@@ -114,8 +139,9 @@ window.reac.run = (root, render, initialstate) => {
         else if (name.startsWith("on") && typeof attribute === "function")
             return event => {
                 let stateHasJustBeenChanged = true // assume change for now
+                const preventUpdate = () => stateHasJustBeenChanged = false
                 
-                const returned = attribute(state, event, () => stateHasJustBeenChanged = false)
+                const returned = attribute(state, event, preventUpdate)
                 if (returned !== undefined) state = returned
     
                 stateHasChanged = stateHasChanged || stateHasJustBeenChanged
@@ -129,19 +155,15 @@ window.reac.run = (root, render, initialstate) => {
     }
 
     function createNativeElement(element){
-        compileView(element)
-
         const native = element.namespace == null? 
             document.createElement(element.tag) :
             document.createElementNS(element.tag, element.namespace)
 
-        if (element.attributes != null)
-            for (let attributeName in element.attributes) 
-                native[attributeName] = createNativeAttribute(attributeName, element.attributes[attributeName])   
+        for (let attributeName in element.attributes || {}) 
+            native[attributeName] = createNativeAttribute(attributeName, element.attributes[attributeName])   
         
-        if (hasChildren(element))
-            for (let child of element.children)
-                native.appendChild(createNativeElement(child))
+        for (let child of element.children || [])
+            native.appendChild(createNativeElement(child))
 
         return native
     }
